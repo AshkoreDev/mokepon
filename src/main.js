b1ab19292c03e2837606eb808b2a291b3d536d75
Aracely Alba <aracelyalba8@gmail.com>
2022-08-21 00:02:47 -0430

    Added chosen pets code

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 
 // Pets
-const hipodoge = document.getElementById('hipodoge');
-const capipepo = document.getElementById('capipepo');
-const ratigueya = document.getElementById('ratigueya');
+const hipodogeInput = document.getElementById('hipodogeInput');
+const capipepoInput = document.getElementById('capipepoInput');
+const ratigueyaInput = document.getElementById('ratigueyaInput');
+
+const playerChosenPet = document.getElementById('playerChosenPet');
 
 // Buttons
 const petBtn = document.getElementById('petBtn');
@@ -13,18 +15,53 @@ const restartBtn = document.getElementById('restartBtn');
 
 
 // Functions
-function petChoice(argument) {
+function random(min, max) {
 	
-	if (hipodoge.checked) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
+function chooseOpponentPet() {
+
+	let opponentAttack = random(1,3);
+	let petOpponent = '';
+
+	if (opponentAttack == 1) {
+
+		petOpponent = 'HIPODOGE';
+		alert('LA MASCOTA DE TU OPONENTE SELECCIONADA ES HIPODOGE.');
+
+	} else if (opponentAttack == 2) {
+
+		petOpponent = 'CAPIPEPO';
+		alert('LA MASCOTA DE TU OPONENTE SELECCIONADA ES CAPIPEPO.');
+
+	} else {
+
+		petOpponent = 'RATIGUEYA';
+		alert('LA MASCOTA DE TU OPONENTE SELECCIONADA ES RATIGUEYA.');
+	}
+
+	opponentChosenPet.textContent = petOpponent;
+}
+
+
+function choosePlayerPet(argument) {
+	
+	let petPlayer = '';
+
+	if (hipodogeInput.checked) {
+
+		petPlayer = 'HIPODOGE';
 		alert('TU MASCOTA SELECCIONADA ES HIPODOGE.');
 
-	} else if (capipepo.checked) {
+	} else if (capipepoInput.checked) {
 		
+		petPlayer = 'CAPIPEPO';
 		alert('TU MASCOTA SELECCIONADA ES CAPIPEPO.');
 
-	} else if (ratigueya.checked) {
+	} else if (ratigueyaInput.checked) {
 		
+		petPlayer = 'RATIGUEYA';
 		alert('TU MASCOTA SELECCIONADA ES RATIGUEYA.');
 
 	} else {
@@ -32,6 +69,9 @@ function petChoice(argument) {
 		alert('SELECCIONA UNA MASCOTA.');
 	}
 
+	playerChosenPet.textContent = petPlayer;
+
+	chooseOpponentPet();
 }
 
-petBtn.addEventListener('click', petChoice);
+petBtn.addEventListener('click', choosePlayerPet);
